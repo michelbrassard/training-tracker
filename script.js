d31a5a84edfe0aa9001d31e2f99d471b58db06fe
Michel Brassard <mb6149@rit.edu>
2024-08-04 09:59:21 +0200

    add line that represents kilometers

--- script.js
+++ script.js
@@ -1,28 +1,89 @@
 document.addEventListener("DOMContentLoaded", () => {
     let canvas = document.getElementById('canvas')
     let context = canvas.getContext('2d');
+    let startX;
+    let startY;
+    let clicked = false;
+    let finished = false;
+    
 
-    //test
-    context.fillRect(50, 50, 200, 100);
+    function resizeCanvas() {
+        let dpr = window.devicePixelRatio || 1;
 
-    ctx.font = '30px Arial';
-    ctx.fillText('Does it work?', 50, 450);
+        let screenWidth = window.innerWidth;
+        let screenHeight = window.innerHeight;
+
+        canvas.width = screenWidth * dpr;
+        canvas.height = screenHeight * dpr;
+
+        canvas.style.width = screenWidth + 'px';
+        canvas.style.height = screenHeight + 'px';
+
+        context.scale(dpr, dpr);
+
+        draw();
+    }
+    function draw() {
+        context.clearRect(0, 0, canvas.width, canvas.height);
+
+        //samo da znam da radi
+        context.fillRect(50, 50, 20, 10);
+    }
+    function drawLine(x1, y1, x2, y2) {
+        context.beginPath(); // Start a new path
+        context.moveTo(x1, y1); // Move to the start point
+        context.lineTo(x2, y2); // Draw a line to the end point
+        context.strokeStyle = 'red'; // Line color
+        context.lineWidth = 10; // Line width
+        context.stroke(); // Render the line
+    }
+
+    resizeCanvas();
+
+    window.addEventListener('resize', resizeCanvas);
 
     canvas.addEventListener('mousedown', (event) => {
         let rectangle = canvas.getBoundingClientRect();
-        let x = event.clientX - rectangle.left;
-        let y = event.clientY - rectangle.top;
+        startX = event.clientX - rectangle.left;
+        startY = event.clientY - rectangle.top;
+        clicked = true;
 
-        context.fillRect(x, y, 50, 50);
     });
 
-    canvas.addEventListener('mouseover', (event) => {
+    canvas.addEventListener('mousemove', (event) => {
         let rectangle = canvas.getBoundingClientRect();
         let x = event.clientX - rectangle.left;
         let y = event.clientY - rectangle.top;
+        
+        if(finished) {
+            return;
+        }
+        if(!clicked) {
+            return;
+        }
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        context.lineCap = 'round';
+        drawLine(startX, startY, x, startY);
 
-        context.strokeRect(x, y, 50, 50);
+        let symbolicKilometers = (x - startX) * 10;
+        
+        context.font = '12px Arial';
+        context.fillStyle = 'red'; 
+        if(symbolicKilometers > 0) {
+            context.fillText(symbolicKilometers, x - 10, startY + 20);
+        }
+        else {
+            symbolicKilometers = symbolicKilometers * (-1);
+            context.fillText(symbolicKilometers, startX - 10, startY + 20);
+        }
+        
     });
+
+    canvas.addEventListener('mouseup', (event) => {
+        clicked = false;
+        finished = true;
+    })
 
     
 });
+
